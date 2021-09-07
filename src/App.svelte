f6af2b9fa5d39c87fb667080245a18e50a1e8f47
breadA <dhairysrivastava5@gmail.com>
2021-09-07 21:48:11 +0530

    add some features

--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,29 @@
   import Toggler from "./ViewMode.svelte";
 </script>
 
-<meta name="viewport" content="width=device-width,initial-scale=1" />
+<div class="animate__fadeIn">
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+  <title>Svelte app</title>
+  <ul id="menu">
+    <li><a href="/">Home</a></li>
+    <li><a href="/">About me</a></li>
+    <li><Toggler>Toggle</Toggler></li>
+  </ul>
 
-<title>Svelte app</title>
-<ul id="menu">
-  <li><a href="/">Home</a></li>
-  <li><a href="/">About me</a></li>
-  <li><Toggler>Toggle</Toggler></li>
-</ul>
-
-<main>
-  <h1>Hello!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
-</main>
+  <main>
+    <h1 class="animate__animated animate__tada" on:mousedown={animateMe}>
+      Hello!
+    </h1>
+    <p>
+      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+      how to build Svelte apps.
+    </p>
+  </main>
+</div>
 
 <style lang="scss">
   @import "./styles/global.scss";
