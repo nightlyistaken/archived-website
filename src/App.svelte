4e8511b7a8b55d978524c10913931ec83bf53244
breadA <dhairysrivastava5@gmail.com>
2021-09-07 15:25:03 +0530

    Use sass and improve buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script lang="ts">
-	export let name: string;
+  import Toggler from "./ViewMode.svelte";
 </script>
 
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+
+<title>Svelte app</title>
+<ul id="menu">
+  <li><a href="/">Home</a></li>
+  <li><a href="/">About me</a></li>
+  <li><Toggler>Toggle</Toggler></li>
+</ul>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style lang="scss">
+  @import "./styles/global.scss";
+  @import "./styles/_darkMode.scss";
 </style>
---
+++ src/ViewMode.svelte
@@ -0,0 +1,13 @@
+<script>
+  function toggle() {
+    window.document.body.classList.toggle("dark-mode");
+  }
+</script>
+
+<button on:click={toggle} class="toggler">
+  <slot />
+</button>
+
+<style lang="scss">
+  @import "./styles/_viewModeButton.scss";
+</style>
